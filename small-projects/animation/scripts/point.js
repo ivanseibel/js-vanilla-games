2920ceface46af82265b278bf6d4db41c1425c0e
Ivan L. Seibel <me@ivanseibel.dev>
2021-11-20 20:03:26 +0000

    Change direction automatically

--- small-projects/animation/scripts/point.js
+++ small-projects/animation/scripts/point.js
@@ -1,18 +1,25 @@
 var currentX = 100;
 var currentY = 100;
 
+var incrementX = 5;
+var incrementY = 5;
+
 const animate = () => {
   const ball = document.querySelector('#ball');
   ball.style.left = currentX + 'px';
   ball.style.top = currentY + 'px';
 
-  currentX += 5;
-  currentY += 5;
+  currentX += incrementX;
+  currentY += incrementY;
 
-  if (currentX > window.innerWidth - 60 || currentY > window.innerHeight - 60) {
-    currentX = 100;
-    currentY = 100;
-  }
+  if (currentX > window.innerWidth - 60 || currentX < 0)
+    incrementX *= -1;
+
+  if (currentY > window.innerHeight - 60 || currentY < 0)
+    incrementY *= -1;
+
+
+  console.log(currentX, currentY);
 
   setTimeout(() => {
     animate();
